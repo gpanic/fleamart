e1549a4f357b93b339075b5801783bb1e8aafae5
Gregor Panič <vision810@gmail.com>
2012-12-13 12:58:43 +0100

    add wcf project

---
+++ fleamart-wcf/Fleamart.Service/FleamartService.svc.cs
@@ -0,0 +1,19 @@
+﻿using Fleamart.Contracts.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Fleamart.Service
+{
+    public class FleamartService : IFleamartService
+    {
+        public string TestOperation()
+        {
+            return "test";
+        }
+    }
+}
---
+++ fleamart-wcf/Fleamart.TestClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using Fleamart.TestClient.FleamartServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fleamart.TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FleamartServiceClient client = new FleamartServiceClient();
+            string response = client.TestOperation();
+            Console.WriteLine(response);
+            Console.ReadLine();
+        }
+    }
+}
