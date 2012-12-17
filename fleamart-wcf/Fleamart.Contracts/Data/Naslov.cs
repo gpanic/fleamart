6a37f5f096e92e76347ac64955725932b749e23e
Dejan Svetec <dejan.svetec0@gmail.com>
2012-12-18 00:08:04 +0100

    contracts.data changes, oglas v0.000001

--- fleamart-wcf/Fleamart.Contracts/Data/Naslov.cs
+++ fleamart-wcf/Fleamart.Contracts/Data/Naslov.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Fleamart.Contracts.Data
 {
+    [DataContract(Name = "Naslov", Namespace = "http//www.fleamart.com/")]
     public class Naslov
     {
+        [DataMember]
         public int Id { get; set; }
+
+        [DataMember]
         public string Ulica { get; set; }
+
+        [DataMember]
         public int PostnaStevilka { get; set; }
+
+        [DataMember]
         public string Obcina { get; set; }
+
+        [DataMember]
         public string Drzava { get; set; }
     }
 }
